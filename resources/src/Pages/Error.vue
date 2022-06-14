328dd16129dbacc549ffbccf0d9977a5810c22ee
amdl89 <amuldahal89@gmail.com>
2022-06-14 14:45:02 +0545

    fixed error page

--- resources/src/Pages/Error.vue
+++ resources/src/Pages/Error.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <H1>{{ title }}</H1>
-    <div>{{ description }}</div>
+  <div class="con">
+    <div>
+      <H1>{{ title }}</H1>
+    </div>
   </div>
 </template>
 
@@ -13,20 +14,21 @@ export default {
   computed: {
     title() {
       return {
-        503: '503: Service Unavailable',
-        500: '500: Server Error',
-        404: '404: Page Not Found',
-        403: '403: Forbidden',
-      }[this.status]
-    },
-    description() {
-      return {
-        503: 'Sorry, we are doing some maintenance. Please check back soon.',
-        500: 'Whoops, something went wrong on our servers.',
-        404: 'Sorry, the page you are looking for could not be found.',
-        403: 'Sorry, you are forbidden from accessing this page.',
-      }[this.status]
+        503: "503: Service Unavailable",
+        500: "500: Server Error",
+        404: "404: Page Not Found",
+        403: "403: Forbidden",
+      }[this.status];
     },
   },
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.con {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
