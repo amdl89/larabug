a3b16a65af40926e8133ee44635064f9b6ab8ada
Amul Dahal <amuldahal89@gmail.com>
2023-11-05 14:33:57 +0545

    changes for vercel deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,32 +22,6 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
-        if (env('APP_ENV') == 'production') {
-            URL::forceScheme('https');
-        }
-
         Schema::defaultStringLength(191);
-
-        // Storage::extend(
-        //     'google',
-        //     function ($app, $config) {
-        //         $options = [];
-
-        //         if (!empty($config['teamDriveId'] ?? null)) {
-        //             $options['teamDriveId'] = $config['teamDriveId'];
-        //         }
-
-        //         $client = new \Google\Client();
-        //         $client->setClientId($config['clientId']);
-        //         $client->setClientSecret($config['clientSecret']);
-        //         $client->refreshToken($config['refreshToken']);
-
-        //         $service = new \Google\Service\Drive($client);
-        //         $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
-        //         $driver = new \League\Flysystem\Filesystem($adapter);
-
-        //         return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
-        //     }
-        // );
     }
 }
