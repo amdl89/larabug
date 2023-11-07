120921f900da34c882dbc99ddfb36e372e689786
Amul Dahal <amuldahal89@gmail.com>
2023-11-07 20:43:00 +0545

    changes for https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,11 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function boot() {
         Schema::defaultStringLength(191);
+
+        // if ($this->app->environment('production')) {
+        //     URL::forceScheme('https');
+        // }
+
+        URL::forceScheme('https');
     }
 }
